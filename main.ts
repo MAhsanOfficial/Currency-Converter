2db197afea1dff561daefbd347cea0181972cac1
Muhammad Ahsan <idahsanali@gmail.com>
2024-04-07 00:15:35 -0700

    this is based of exchange currency rate to dollar and gbp ,inr .pkr ,euro

---
+++ main.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const Currency : any= {
+USD:1,   // Base Currency
+EUR:0.92,
+GBP:0.79,
+INR:74.58,
+PKR:277.0
+}
+
+let userAnswer = await inquirer.prompt([
+    {
+name:"from",
+message:"Enter From Currency",
+type:"list",
+choices:["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name:"to",
+    message:"Enter To Currency",
+    type:"list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+    },
+    {
+        name:"amount",
+        message:"Enter Your Amount",
+        type:"number",
+        }
+])
+let fromAmount = Currency[userAnswer.from] // Exchange Rate
+let toAmount = Currency[userAnswer.to]    // Exchange Rate
+let amount = userAnswer.amount
+// let result = amount * fromAmount / toAmount 
+let  baseAmount = amount / fromAmount // USD Base Currency
+let result = baseAmount * toAmount
+console.log(result)
+
